73841e8d0c6a191a5a1996e83b5c4e4640ea1ca1
Aman Kumar <aman.kumar24220@gmail.com>
2022-06-18 02:39:57 +0530

    AppleTv

---
+++ Inner Pages/index.js
@@ -0,0 +1,19 @@
+document.querySelector("#season3").addEventListener("click", Season3fun);
+function Season3fun(){
+
+window.location.href = "video.html";
+}
+document.querySelector("#Joel").addEventListener("click", Joelfun);
+function Joelfun(){
+    window.location.href = "cast&crew.html"
+}
+document.querySelector("#AppleTV").addEventListener("click", Applefun);
+function Applefun(){
+window.location.href = "AppleTv.html"
+}
+document.querySelector("#logo").addEventListener("click", Logofun);
+
+function Logofun(){
+window.location.href = "index.html"
+
+}
